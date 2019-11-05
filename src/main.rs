313c6576bbd68e71cb79bbb534f5f86e72871af9
Sebastian Dröge <sebastian@centricular.com>
2019-11-05 14:14:22 +0100

    Chapter 4 - Joining a session and message loop

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use structopt::StructOpt;
 
 use tokio::prelude::*;
 
+use tungstenite::Error as WsError;
 use tungstenite::Message as WsMessage;
 
 use anyhow::{anyhow, bail};
@@ -12,6 +13,48 @@ use anyhow::{anyhow, bail};
 struct Args {
     #[structopt(short, long, default_value = "wss://webrtc.nirbheek.in:8443")]
     server: String,
+    #[structopt(short, long)]
+    peer_id: u32,
+}
+
+async fn run(
+    ws: impl Sink<WsMessage, Error = WsError> + Stream<Item = Result<WsMessage, WsError>>,
+) -> Result<(), anyhow::Error> {
+    // Split the websocket into the Sink and Stream
+    let (mut ws_sink, ws_stream) = ws.split();
+    // Fuse the Stream, required for the select macro
+    let mut ws_stream = ws_stream.fuse();
+
+    // And now let's start our message loop
+    loop {
+        let ws_msg = futures::select! {
+            // Handle the WebSocket messages here
+            ws_msg = ws_stream.select_next_some() => {
+                match ws_msg? {
+                    WsMessage::Close(_) => {
+                        println!("peer disconnected");
+                        break
+                    },
+                    WsMessage::Ping(data) => Some(WsMessage::Pong(data)),
+                    WsMessage::Pong(_) => None,
+                    WsMessage::Binary(_) => None,
+                    WsMessage::Text(text) => {
+                        println!("received text: {}", text);
+                        None
+                    },
+                }
+            },
+            // Once we're done, break the loop and return
+            complete => break,
+        };
+
+        // If there's a message to send out, do so now
+        if let Some(ws_msg) = ws_msg {
+            ws_sink.send(ws_msg).await?;
+        }
+    }
+
+    Ok(())
 }
 
 #[tokio::main]
@@ -38,7 +81,19 @@ async fn main() -> Result<(), anyhow::Error> {
         bail!("server didn't say HELLO");
     }
 
-    println!("received {:?}", msg);
+    // Join the given session
+    ws.send(WsMessage::Text(format!("SESSION {}", args.peer_id)))
+        .await?;
 
-    Ok(())
+    let msg = ws
+        .next()
+        .await
+        .ok_or_else(|| anyhow!("didn't receive anything"))??;
+
+    if msg != WsMessage::Text("SESSION_OK".into()) {
+        bail!("server error: {:?}", msg);
+    }
+
+    // All good, let's run our message loop
+    run(ws).await
 }
