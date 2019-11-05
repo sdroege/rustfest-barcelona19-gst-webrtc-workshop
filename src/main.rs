29312a38d2a9db25a5e7231f6050dc34fcd40082
Sebastian Dröge <sebastian@centricular.com>
2019-11-05 13:28:48 +0100

    Chapter 2 - Command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use structopt::StructOpt;
+
 use tokio::prelude::*;
+
+#[derive(Debug, StructOpt)]
+struct Args {
+    #[structopt(short, long, default_value = "wss://webrtc.nirbheek.in:8443")]
+    server: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    let args = Args::from_args();
+
+    println!("args: {:?}", args);
 
     Ok(())
 }
